c63f5c614fde6b6e076417a78e8f2542f4931fcd
Folkert Schram de Jong <folkert.schramdejong@capgemini.com>
2015-12-01 16:40:46 +0100

    Added request mapping

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -5,15 +5,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.stereotype.*;
 
-@Controller
+import java.util.Collections;
+import java.util.Map;
+import java.util.UUID;
+
+import javax.servlet.http.HttpSession;
+
+@RestController
 @SpringBootApplication
 public class DemoApplication {
 
-    @RequestMapping("/")
-    @ResponseBody
-    String home() {
-      return "Hello World!";
-    }
+    @RequestMapping("/random")
+	public Map<String, String> token(HttpSession session) {
+		return Collections.singletonMap("id", UUID.randomUUID().toString());
+	}
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
